48f46f6baa943f144046e221fdfb11ea1176ca0d
Sébastien Lavoie <github@lavoie.sl>
2014-03-17 11:30:46 -0400

    updated for new AppLoader behaviour

--- bin/symfony-kickstart-update-composer.json.php
+++ bin/symfony-kickstart-update-composer.json.php
@@ -3,13 +3,16 @@ $json_path = "${argv[1]}/composer.json";
 $json = file_get_contents($json_path);
 $data = json_decode($json,true);
 //$data["require"]["wemakecustom/symfony-app-loader"] = "~1.0@dev";
-$data["scripts"]["post-install-cmd"][] = "WMC\AppLoader\ScriptHandler::buildParameters";
-$data["scripts"]["post-update-cmd"][] = "WMC\AppLoader\ScriptHandler::buildParameters";
+foreach (array('post-install-cmd', 'post-update-cmd') as $event) {
+    $data['scripts'][$event][] = "WMC\Composer\Utils\ConfigFile\ConfigDir::updateDirs";
+    $data['scripts'][$event][] = "WMC\AppLoader\ScriptHandler::buildParameters";
+}
 $data["minimum-stability"] = "dev";
 $data["prefer-stable"] = true;
 $data["extra"]["symfony-app-dir"] = "app";
 $data["extra"]["symfony-web-dir"] = "htdocs";
 $data["extra"]["symfony-assets-install"] = "relative";
+$data["extra"]["update-config-dirs"]["confs/dist"] = "confs";
 $data["repositories"][] = array(
 	"type" => "composer",
 	"url" => "http://gitlab-composer.stage.wemakecustom.com/"
