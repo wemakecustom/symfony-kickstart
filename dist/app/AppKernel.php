27cb38d3018456a754ab25ab761d05ba4eacc806
Christophe Faribault <christophe@wemakecustom.com>
2014-12-23 16:49:48 -0500

    Update AppKernel.php

--- dist/app/AppKernel.php
+++ dist/app/AppKernel.php
@@ -19,6 +19,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            
+            new Sp\BowerBundle\SpBowerBundle(),
 
             new JMS\AopBundle\JMSAopBundle,
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle,
